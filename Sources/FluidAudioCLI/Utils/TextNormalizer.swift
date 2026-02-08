7df7e9cdbffae2a9f6132b2cf46a7f7bc44f54c4
Alex-Wengg <hanweng9@gmail.com>
2026-02-08 11:40:22 -0500

    fix: remove duplicate 'six' key in TextNormalizer numberWords dictionary
    The key "six": "6" was present in both English and French sections,
    causing a Swift runtime crash on dictionary initialization.

--- Sources/FluidAudioCLI/Utils/TextNormalizer.swift
+++ Sources/FluidAudioCLI/Utils/TextNormalizer.swift
@@ -397,7 +397,7 @@ enum TextNormalizer {
 
             // French numbers
             "zéro": "0", "un": "1", "deux": "2", "trois": "3", "quatre": "4",
-            "cinq": "5", "six": "6", "sept": "7", "huit": "8", "neuf": "9",
+            "cinq": "5", "sept": "7", "huit": "8", "neuf": "9",
             "dix": "10", "onze": "11", "douze": "12", "treize": "13", "quatorze": "14",
             "quinze": "15", "seize": "16", "dix-sept": "17", "dix-huit": "18",
             "dix-neuf": "19", "vingt": "20", "trente": "30", "quarante": "40",
